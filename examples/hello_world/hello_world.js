e52c7ece553ba0012ce218bda0c8936614c46d25
pp-jpierun <109102095+pp-jpierun@users.noreply.github.com>
2022-11-24 18:21:49 +0100

    Update hello_world.js

--- examples/hello_world/hello_world.js
+++ examples/hello_world/hello_world.js
@@ -29,9 +29,11 @@ looker.plugins.visualizations.add({
         }
         .hello-world-text-large {
           font-size: 72px;
+          font-size_pop: 22px;
         }
         .hello-world-text-small {
           font-size: 18px;
+          font-size-pop: 18px;
         }
       </style>
     `;
@@ -42,6 +44,7 @@ looker.plugins.visualizations.add({
 
     // Create an element to contain the text.
     this._textElement = container.appendChild(document.createElement("div"));
+    this._textElement2 = container.appendChild(document.createElement("div2"));
 
   },
   // Render in response to the data or settings changing
@@ -58,16 +61,28 @@ looker.plugins.visualizations.add({
 
     // Grab the first cell of the data
     var firstRow = data[0];
-    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+    var firstCell = firstRow[queryResponse.fields.measures[0].name];
+    var firstDim = firstRow[queryResponse.fields.dimensions[0].name];
+    
 
     // Insert the data into the page
-    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
+    // This would be unstyled, you'd have to actually construct the HTML string if you wanted it styled
+    this._textElement.innerHTML = firstCell;
+    this._textElement2.innerHTML = firstDim;
+    // Insert the data into the page
+    //this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
 
     // Set the size to the user-selected size
     if (config.font_size == "small") {
       this._textElement.className = "hello-world-text-small";
     } else {
       this._textElement.className = "hello-world-text-large";
+    }
+    
+    if (config.font_size_pop == "small") {
+      this._textElement2.className = "hello-world-text-small";
+    } else {
+      this._textElement2.className = "hello-world-text-large";
     }
 
   }
